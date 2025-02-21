b93d023dd781f539b8edab0f2bca855ff27c2e65
linawine <lina.vladimirova15@mail.ru>
2025-02-21 09:28:35 +0500

    Correct mistakes: CSS

--- js/company-slider.js
+++ js/company-slider.js
@@ -1,8 +1,21 @@
-$(document).ready(function () {
-    $('.company-slider').slick({
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1
+$(document).ready(function() {
+  let images = $('.company-slider img');
+  let loadedCount = 0;
+
+  images.each(function() {
+    $(this).on('load', function() {
+      loadedCount++;
+      if (loadedCount === images.length) {
+        $('.company-slider').slick({
+          dots: true,
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        });
+      }
     });
+    if (this.complete) {
+      $(this).trigger('load');
+    }
   });
+});
